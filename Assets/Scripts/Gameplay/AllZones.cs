fa17a527426a1aaa29e0f2d86282ae267dcdebc4
Andrii Gastello <concof.zp@gmail.com>
2025-07-31 14:23:50 -0400

    Bugfix

--- Assets/Scripts/Gameplay/AllZones.cs
+++ Assets/Scripts/Gameplay/AllZones.cs
@@ -4,13 +4,13 @@ namespace LamentsNeverEnd.Scripts.Gameplay
 {
     public static class AllZones
     {
-        public List<ZoneInfo> AllZones = new List<ZoneInfo>()
+        public static List<ZoneInfo> allZones { get; } = new List<ZoneInfo>()
         {
             new ZoneInfo(CycleZoneID.Penek)
             {
                 Left = CycleZoneID.Zone1,
-                Right =  CycleZoneID.Zone2,
-            }, 
+                Right = CycleZoneID.Zone2,
+            },
             new ZoneInfo(CycleZoneID.Zone1)
             {
                 Up = CycleZoneID.Penek,

0027a67f161f99a508d94033e9ef320b4f4a86d9
Stas Voronov <stasv2003@gmail.com>
2025-07-30 23:49:20 -0400

    kakayato huyna

---
+++ Assets/LamentsNeverEnd/Scripts/Gameplay/CyclingZone.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using LamentsNeverEnd.Scripts;
+using LamentsNeverEnd.Scripts.Gameplay;
+using UnityEngine;
+
+public class CyclingZone : MonoBehaviour
+{
+    [SerializeField] private List<TriggerZone> zones;
+    [SerializeField] public string zoneName;
+    
+    public string ZoneName => zoneName;
+    public event EventHandler<ZoneSide> OnZoneEntered;
+    private void Awake()
+    {
+        foreach (var zone in  zones)
+        {
+            zone.OnZoneEnter += OnZoneEnter;
+        }
+    }
+
+    private void OnZoneEnter(object sender, ZoneSide e)
+    {
+        Debug.Log($"Location {ZoneName} entered form side:  {e}");
+        OnZoneEntered?.Invoke(this, e);
+    }
+}

9bcdc9dc09f8d706a827ba1d46dd02b8210aca4b
Andrii Gastello <concof.zp@gmail.com>
2025-08-02 15:27:54 -0400

    Torch controls + flickering (with bugs)

--- Assets/Scripts/Utility/TorchController.cs
+++ Assets/Scripts/Utility/TorchController.cs
@@ -1,84 +1,91 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
-using UnityEngine.UIElements;
 
 namespace Utility
 {
     public class TorchController : MonoBehaviour
     {
-        private Light2D _torchlight;
-        public TorchLight torchlight;
+        public LightController torchlight;
 
-        [SerializeField] InputActionAsset inputActions;
+        [SerializeField] private InputActionAsset inputActions;
 
-        [SerializeField] TorchData _torchData;
-
-        private bool _isOn = true;
-        private float _targetIntensity;
+        [SerializeField] private TorchData torchData;
 
         // Input Actions
         private InputAction _dimAction;
         private InputAction _normalAction;
         private InputAction _toggleAction;
-        private void Awake()
-        {
-        }
+        
+        private Light2D _torchlight;
+        private bool _isOn = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
         {
             if (!_torchlight)
                 _torchlight = GetComponent<Light2D>();
-            
+
             if (!_torchlight)
                 Debug.LogWarning("Light2D component not found on this GameObject.", this);
-            
+
             if (!torchlight)
-                torchlight = GetComponent<TorchLight>();
-            
-            _targetIntensity = _torchData.normalIntensity;
-            
-            
+                torchlight = GetComponent<LightController>();
+
+
             _toggleAction = inputActions.FindAction("Torch/Toggle");
             _dimAction = inputActions.FindAction("Torch/Dim");
             _normalAction = inputActions.FindAction("Torch/Normal");
-            
+
             _toggleAction.Enable();
             _dimAction.Enable();
             _normalAction.Enable();
-            
+
             _toggleAction.performed += ctx => Toggle();
+
+            // Pass data to LightController
+            if (torchlight)
+                torchlight.torchData = torchData;
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
             if (!_isOn)
                 return;
-            
-            
-            
-            if (_dimAction.ReadValue<float>() > 0) _targetIntensity = _torchData.dimIntensity;
-            if (_normalAction.ReadValue<float>() > 0) _targetIntensity =  _torchData.normalIntensity;
-            
-            _torchlight.intensity = Mathf.MoveTowards(_torchlight.intensity, _targetIntensity,
-                _torchData.transitionSpeed * Time.deltaTime);
-            
-            torchlight.defaultIntensity = _torchlight.intensity;
+
+            if (_dimAction.ReadValue<float>() > 0)
+            {
+                _torchlight.intensity = Mathf.MoveTowards(_torchlight.intensity, torchData.dimIntensity, torchData.transitionSpeed * Time.deltaTime);
+                // target = torchData.flickRange.x;
+                Debug.Log($"torchData.defaultIntensity: {torchData.defaultIntensity}");
+            }
+
+            if (_normalAction.ReadValue<float>() > 0)
+            {
+                // target = torchData.flickRange.y;
+                _torchlight.intensity = Mathf.MoveTowards(_torchlight.intensity, torchData.normalIntensity, torchData.transitionSpeed * Time.deltaTime);
+                Debug.Log($"torchData.defaultIntensity: {torchData.defaultIntensity}");
+            }
+
+            // _torchlight.intensity = Mathf.MoveTowards(_torchlight.intensity, target, torchData.transitionSpeed * Time.deltaTime);
+
+            // if (torchlight)
+            //torchData.defaultIntensity = _torchlight.intensity;
+            Debug.Log($"TorchData.defaultIntensity: {torchData.defaultIntensity}");
         }
 
-        void Toggle()
-        {
-            _isOn = !_isOn;
-            _torchlight.enabled = _isOn;
-        }
-
-        void OnDestroy()
+        private void OnDestroy()
         {
             _dimAction.Disable();
             _normalAction.Disable();
             _toggleAction.Disable();
+        }
+
+        private void Toggle()
+        {
+            _isOn = !_isOn;
+            _torchlight.enabled = _isOn;
         }
     }
 }
--- Assets/Scripts/Utility/TorchData.cs
+++ Assets/Scripts/Utility/TorchData.cs
@@ -1,13 +1,40 @@
 using System;
+using UnityEngine;
 
 namespace Utility
 {
-    [Serializable]
-    public struct TorchData
+    [CreateAssetMenu (fileName = "TorchData", menuName = "Torch Data")]
+    public class TorchData : ScriptableObject
     {
+        [Header("Intensity Ranges")]
+        
+        [Range(0.1f, 1f)]
+        public float minIntensity;
+
+        [Range(0.1f, 1.5f)]
+        public float maxIntensity;
+
+        [Header("Fixed Intensities")]
+        public float defaultIntensity = 1.0f;
         public float dimIntensity;
         public float normalIntensity;
 
+        [Header("Transitions")]
         public float transitionSpeed;
+
+        [Header("Flicker")]
+        public Vector2 flickRange
+        {
+            get => new Vector2(minIntensity, maxIntensity);
+            set
+            {
+                minIntensity = value.x;
+                maxIntensity = value.y;
+            }
+        }
+
+        public float flickTime;
+        public float flickBackTime;
+        public Vector2 flickCooldown;
     }
 }
